0f61e5eb19b0761a6ce66e882b98a9bc0ddb88db
zcamcal <188593322+zcamcal@users.noreply.github.com>
2024-11-21 17:44:07 -0300

    feat: go mod init, tcp socket works

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module arbitrium/server
+
+go 1.23.2
---
+++ server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log/slog"
+	"net"
+	"os"
+)
+
+var logger *slog.Logger
+
+func main() {
+
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	port := flag.String("port", ":9090", "allows to change the port where will connect the slaves")
+	logger.Info("starting tcp server: ", slog.String("port", *port))
+
+	listener, err := net.Listen("tcp", *port)
+	if err != nil {
+		logger.Error("Error when tried to create tcp listener", slog.Any("err", err))
+		return
+	}
+
+	for {
+		accepted, tryToAcceptError := listener.Accept()
+		if tryToAcceptError != nil {
+			logger.Error("something happend when conection intent was accepted", slog.Any("err", tryToAcceptError))
+			continue
+		}
+
+		go handleConnection(accepted)
+	}
+
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+
+	message, err := bufio.NewReader(connection).ReadString('\n')
+	if err != nil {
+		logger.Error("cant read message from slave", slog.Any("err", err))
+    return
+	}
+
+	logger.Info("message received from slave", slog.Any("message", message))
+}
